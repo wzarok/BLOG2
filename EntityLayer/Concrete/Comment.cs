bea08751b77f66277e707942506da63133837689
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-02-24 22:07:52 +0300

    BLOG2

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+        [StringLength(50)]
+        public string CommentUser { get; set; }
+        [StringLength(50)]
+        public string CommentMail { get; set; }
+        [StringLength(200)]
+        public string CommentText { get; set; }
+
+        public int BlogID { get; set; }
+        public virtual Blog Blog{ get; set; }
+    }
+}
