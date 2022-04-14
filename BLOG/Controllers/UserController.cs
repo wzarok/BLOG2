06a8348fa7a65ab984df982423b89f64bd34f264
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-04-14 23:09:11 +0300

    update1

--- BLOG/Controllers/UserController.cs
+++ BLOG/Controllers/UserController.cs
@@ -1,18 +1,21 @@
 ﻿using BusinnessLayer.Concrete;
 using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace BLOG.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         // GET: User
         UserProfileManager userprofile = new UserProfileManager();
-
+        BlogManager bm = new BlogManager();
 
         public ActionResult Index()
         {
@@ -25,7 +28,14 @@ namespace BLOG.Controllers
             var profilevalues = userprofile.GetAuthorByMail(p);
             return PartialView(profilevalues);
         }
-        public ActionResult BlogList(string p)
+        
+        public ActionResult UpdateUserProfile(Author p)
+        {
+            userprofile.EditAuthor(p);
+            return RedirectToAction("Index");
+        }
+      
+        public ActionResult AuthorBlogList(string p)
         {
             p = (string)Session["Mail"];
             Context c = new Context();
@@ -33,5 +43,73 @@ namespace BLOG.Controllers
             var blogs = userprofile.GetBlogByAuthor(id);
             return View(blogs);
         }
+        [HttpGet]
+        public ActionResult UpdateBlog(int id)
+        {
+
+            Blog blog = bm.FindBlog(id);
+            Context c = new Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
+            List<SelectListItem> aut = (from x in c.Authors.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.AuthorName,
+                                            Value = x.AuthorID.ToString()
+                                        }).ToList();
+            ViewBag.aut = aut;
+            return View(blog);
+        }
+        [HttpPost]
+        public ActionResult UpdateBlog(Blog p)
+        {
+            bm.UpdateBlog(p);
+            return RedirectToAction("AuthorBlogList");
+        }
+        public ActionResult DeleteBlog(int id)
+        {
+            bm.DeleteBlogBL(id);
+            return RedirectToAction("AuthorBlogList");
+        }
+        [HttpGet]
+        public ActionResult AddNewBlog()
+        {
+            Context c = new Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
+            List<SelectListItem> aut = (from x in c.Authors.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.AuthorName,
+                                            Value = x.AuthorID.ToString()
+                                        }).ToList();
+            ViewBag.aut = aut;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddNewBlog(Blog b)
+        {
+
+            bm.BlogAddBL(b);
+            return RedirectToAction("AuthorBlogList");
+        }
+        
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("AuthorIndex", "Login");
+        }
+
     }
 }
--- BusinnessLayer/Concrete/UserProfileManager.cs
+++ BusinnessLayer/Concrete/UserProfileManager.cs
@@ -11,7 +11,7 @@ namespace BusinnessLayer.Concrete
     public class UserProfileManager
     {
         Repository<Author> repouser = new Repository<Author>();
-        Repository<Blog> repouserblog = new Repository<Blog>();
+        Repository<Blog> repouserblog = new Repository<Blog>(); 
         public List<Author> GetAuthorByMail(string p)
         {
             return repouser.List(x => x.AuthorMail == p);
@@ -19,6 +19,19 @@ namespace BusinnessLayer.Concrete
         public List<Blog> GetBlogByAuthor(int id)
         {
             return repouserblog.List(x => x.AuthorID == id);
+        }
+        public int EditAuthor(Author p)
+        {
+            Author author = repouser.Find(x => x.AuthorID == p.AuthorID);
+            author.AboutShort = p.AboutShort;
+            author.AuthorName = p.AuthorName;
+            author.AuthorPass = p.AuthorPass;
+            author.AuthorTitle = p.AuthorTitle;
+            author.AuthorMail = p.AuthorMail;
+            author.AuthorImage = p.AuthorImage;
+            author.AuthoPhone = p.AuthoPhone;
+            author.AuthorAbout = p.AuthorAbout;
+            return repouser.Update(author);
         }
     }
 }
